c3691742e44b1bc8683844dc6304f79a04c157e7
Mark Meves <mark.meves@gmail.com>
2010-10-20 01:27:06 -0400

    pull in sites, pasengerconf

--- conf.rb
+++ conf.rb
@@ -6,6 +6,8 @@ RoxConf::Conf = {
     { :path => 'userconf' },
     { :path => 'redconf' },
     { :path => 'monitconf.d/monitconf'},
+    { :path => 'passengerconf' },
+    { :path => '/etc/nginx/sites', :cd => '../hipe-sites' },
     { :path => '/etc/thin/thinconf', :cd => :dir },
     { :path => 'uniconf' },
     { :path => '/var/sites/redmine-git/current/script/mineconf', :cd => '../..' },
